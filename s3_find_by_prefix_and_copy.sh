5a1b53e9153e902d3648eb76a1aae80e53b94011
goga <goga@fake.local>
2019-08-12 21:40:05 -0700

    s3 copy example

--- s3_find_by_prefix_and_copy.sh
+++ s3_find_by_prefix_and_copy.sh
@@ -7,7 +7,7 @@ if [ $# -eq 0 ]
     echo "No arguments supplied. Working with manual input..."
     echo "Enter S3 bucket name: (Example: recordings-us-east-1 )"
     read s3_bucket
-    echo "Enter S3 prefix in bucket: (Example: 11496187-222c-47a7-878e-b30eb408e2ee/02BLSMH0ACBF1CP8AIU362LAES009M0S )"
+    echo "Enter S3 prefix in bucket: (Example: somedir/path-to-file or somedir/prefix-for-multiple-files )"
     read s3_prefix
     echo "Result of search: (Output is S3 locations in a bucket)"
     #arr=$(aws s3api list-objects-v2 --bucket $s3_bucket --prefix $s3_prefix --query Contents[].[Size,Key] | xargs)
