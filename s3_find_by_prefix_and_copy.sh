457d2107475b1d1cb9bfaf2c15c027a102fd83cc
goga <goga@fake.local>
2019-08-12 22:16:35 -0700

    s3_find

--- s3_find_by_prefix_and_copy.sh
+++ s3_find_by_prefix_and_copy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# Find files in S3 bucket by PREFIX
+# Find files in S3 bucket by PREFIX and copy them all to temp dir 
+
+TEMP_DIR="s3_find_temp_dir/"
 
 if [ $# -eq 0 ]
   then
@@ -13,12 +15,12 @@ if [ $# -eq 0 ]
     #arr=$(aws s3api list-objects-v2 --bucket $s3_bucket --prefix $s3_prefix --query Contents[].[Size,Key] | xargs)
     arr=$(aws s3api list-objects-v2 --bucket $s3_bucket --prefix $s3_prefix --query Contents[].[Key] | xargs)
     mkdir s3_find_temp
-    for i in $arr; do aws s3 cp s3://$s3_bucket/$i s3_find_temp/; done
-    ls -ltrh s3_find_temp/
+    for i in $arr; do aws s3 cp s3://$s3_bucket/$i $TEMP_DIR; done
+    ls -ltrh $TEMP_DIR
   else
     echo "Working with script arguments..."
     arr=$(aws s3api list-objects-v2 --bucket $1 --prefix $2 --query Contents[].[Key] | xargs)
     mkdir s3_find_temp
-    for i in $arr; do aws s3 cp s3://$1/$i s3_find_temp/; done
-    ls -ltrh s3_find_temp/
+    for i in $arr; do aws s3 cp s3://$1/$i $TEMP_DIR; done
+    ls -ltrh $TEMP_DIR
 fi
